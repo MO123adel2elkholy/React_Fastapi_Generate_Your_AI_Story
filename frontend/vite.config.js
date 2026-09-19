ceb0bfe7e3ec51d87cb3383d838df0ef25404c91
mahmoud adel elkholy <adel333mahmoud@gmail.com>
2026-09-19 02:35:18 +0200

    Fix : Configure Vite server to recive and send data from teh backend server - add server block to allow vite proxy server to conneect with backend server - affected files  components/Vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,45 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
   ],
-})
+  server: {
+    
+      proxy: {
+        "/api": {
+          target: "http://localhost:8000",
+          changeOrigin: true,
+          secure: false
+        }
+      }
+    }
+    }
+)
+
+
+
+
+// import { defineConfig, loadEnv } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig((command, mode) => {
+//   const env = loadEnv(mode, process.cwd(), "")
+
+//   console.log(env.VITE_DEBUG)
+
+//   return {
+//     plugins: [react()],
+//     server: {
+//     ...(env.VITE_DEBUG === "true" && {
+//       proxy: {
+//         "/api": {
+//           target: "http://localhost:8000",
+//           changeOrigin: true,
+//           secure: false
+//         }
+//       }
+//     }
+//     )}
+//   }
+// })
